f2bf2bd1f64061ba444826724c166413b585ba7b
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2017-10-18 14:57:54 +0200

    Knihovna utils pro pomocné funkce a datové struktury

---
+++ Libs/utils.h
@@ -0,0 +1,34 @@
+/******************************************************************************/
+/**
+ * \project IFJ-Compiler
+ * \file    utils.h
+ * \brief   Liblary providing supportive function and types for the project
+ *
+ * \author  Petr Fusek (xfusek08)
+ * \date    18.10.2017 - Petr Fusek
+ */
+/******************************************************************************/
+
+#ifndef _utils
+#define _utils
+
+#include <string.h>
+#include "MMng.h"
+
+/**
+ * Structure of basic dataset of input language data types, 
+ * for storing data of constants or literals ect.
+ */
+typedef struct {
+  int intVal;
+  double doubleVal;
+  char *stringVal;
+} SData; 
+
+/**
+ * Fuction create hard copy of given string
+ * \note Memory is allocated here and free has to be called.
+ */
+char *util_StrHardCopy(const char *str);
+
+#endif // _utils
