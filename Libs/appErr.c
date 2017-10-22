835620b5dcea42a8f4440c243851ba813979065e
Radim Blaha <xblaha28@stud.fit.vutbr.cz>
2017-10-22 02:45:52 +0200

    ApplicationErrors(#2): add colors, change texts of error messages, comments

--- Libs/appErr.c
+++ Libs/appErr.c
@@ -4,39 +4,47 @@
  * \file    appErr.c
  * \brief   Application Errors
  * \author  Radim Blaha (xblaha28)
- * \date    21.10.2017 - Radim Blaha
+ * \date    22.10.2017 - Radim Blaha
  */
 /******************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "appErr.h"
+#include "MMng.h"
 
 void apperr_runtimeError(char *errMsg){
-    fprintf( stderr, "%s\n", errMsg);
-    // TODO: dealloc memory
-    exit(2);
+    fprintf( stderr, "\033[31;1mError:\033[0m %s\n", errMsg);
+    mmng_freeAll();
+    exit(10);
 }
 
 void apperr_codeError(ErrType type, int row, int col, char *line){
+    
     switch (type)
     {
         case lexicalErr:
-            fprintf( stderr, "Error %d: Lexical error caused by %s.\n", type, line);
+            fprintf( stderr, "\033[31;1mError %d:\033[0m Lexical error:\n%s. ", type, line);
+            break;
         case syntaxErr:
-            fprintf( stderr, "Error %d: Syntax error caused by %s.\n", type, line);
+            fprintf( stderr, "\033[31;1mError %d:\033[0m Syntax error:\n%s. ", type, line);
+            break;
         case semanticErr:
-            fprintf( stderr, "Error %d: Semantic error caused by %s.\n", type, line);
+            fprintf( stderr, "\033[31;1mError %d:\033[0m Semantic error:\n%s. ", type, line);
+            break;
         case typeCompatibilityErr:
-            fprintf( stderr, "Error %d: Error of type compatibility caused by %s.\n", type, line);
+            fprintf( stderr, "\033[31;1mError %d:\033[0m Error of type compatibility:\n%s. ", type, line);
+            break;
         case anotherSemanticErr:
-            fprintf( stderr, "Error %d: Another semantic error caused by %s.\n", type, line);
+            fprintf( stderr, "\033[31;1mError %d:\033[0m Another semantic error:\n%s. ", type, line);
+            break;
         case internalErr:
-            fprintf( stderr, "Error %d: Internal compiler error caused by %s.\n", type, line);
+            fprintf( stderr, "\033[31;1mError %d:\033[0m Internal compiler error:\n%s. ", type, line);
+            break;
         default:
-            return 0;
+            return;
     }
 
-    fprintf( stderr, "On Ln %d, Col %d.\n", row, col);
+    fprintf( stderr, "\033[33mOn Ln %d, Col %d.\033[0m\n", row, col);
     exit(type);
 }
--- Libs/appErr.h
+++ Libs/appErr.h
@@ -4,36 +4,45 @@
  * \file    appErr.h
  * \brief   Application Errors
  *
- * Long decsription will be placed here.
+ * Library what processes application errors.
  *
  * \author  Radim Blaha (xblaha28)
- * \date    21.10.2017 - Radim Blaha
+ * \date    22.10.2017 - Radim Blaha
  */
 /******************************************************************************/
 
 #ifndef appErr
 #define appErr
 
+/*! Enum of error types and error codes */
 typedef enum {
-    lexicalErr = 1,
-    syntaxErr = 2,
-    semanticErr = 3,
-    typeCompatibilityErr = 4,
-    anotherSemanticErr = 6,
-    internalErr = 99
+    lexicalErr = 1, /*!< incorrect structure of the current lex */
+    syntaxErr = 2,  /*!< wrong program syntax */
+    semanticErr = 3,    /*!< undefined function/variable, attempt to redefine function/variable, etc. */
+    typeCompatibilityErr = 4,   /*!< semantic error of type compatibility in arithmetic, string and relational */
+                                /*!< expressions, or poor number of parameter types for function calls */
+    anotherSemanticErr = 6, /*!< another semantic error */
+    internalErr = 99    /*!< internal compiler error - unaffected by the input program (eg. memory allocation error, etc.) */
 } ErrType;
 
 /**
- * \brief Initialization
+ * \brief Prints errors
  *
- * Long desc.
+ * Print errors another then defined and terminates program with error code 10.
+ *
+ * \param errMsg array of chars (string) containg error message
  */
 void apperr_runtimeError(char *errMsg);
 
 /**
- * \brief Initialization
+ * \brief Print error and terminates program
  *
- * * Long desc.
+ * Function prints compiling error and terminates program.
+ *
+ * \param type defined error codes (ErrType enum)
+ * \param row row where the error occurred
+ * \param col column where the error occurred
+ * \param line wrong section of code
  */
 void apperr_codeError(ErrType type, int row, int col, char *line);
 
