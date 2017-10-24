52a8ce8a4818a634fd37296bb2ed238da13f3be8
Radim Blaha <xblaha28@stud.fit.vutbr.cz>
2017-10-24 19:36:25 +0200

    ApplicationErrors(#2): edit tab spacing - second attempt (sorry for git testing)

--- Libs/appErr.c
+++ Libs/appErr.c
@@ -15,61 +15,61 @@
 #include "MMng.h"
 
 void apperr_runtimeError(char *errMsg){
-    fprintf( stderr, "\033[31;1mError:\033[0m %s\n", errMsg);
-    mmng_freeAll();
-    exit(internalErr);
+  fprintf( stderr, "\033[31;1mError:\033[0m %s\n", errMsg);
+  mmng_freeAll();
+  exit(internalErr);
 }
 
 void apperr_codeError(ErrType type, int row, int col, char *line){
     
-    switch (type)
-    {
-        case lexicalErr:
-            fprintf( stderr, "\033[31;1mError %d:\033[0m Lexical error ", type);
-            break;
-        case syntaxErr:
-            fprintf( stderr, "\033[31;1mError %d:\033[0m Syntax error ", type);
-            break;
-        case semanticErr:
-            fprintf( stderr, "\033[31;1mError %d:\033[0m Semantic error ", type);
-            break;
-        case typeCompatibilityErr:
-            fprintf( stderr, "\033[31;1mError %d:\033[0m Error of type compatibility ", type);
-            break;
-        case anotherSemanticErr:
-            fprintf( stderr, "\033[31;1mError %d:\033[0m Another semantic error ", type);
-            break;
-        default:
-            return;
+  switch (type)
+  {
+    case lexicalErr:
+      fprintf( stderr, "\033[31;1mError %d:\033[0m Lexical error ", type);
+      break;
+    case syntaxErr:
+      fprintf( stderr, "\033[31;1mError %d:\033[0m Syntax error ", type);
+      break;
+    case semanticErr:
+      fprintf( stderr, "\033[31;1mError %d:\033[0m Semantic error ", type);
+      break;
+    case typeCompatibilityErr:
+      fprintf( stderr, "\033[31;1mError %d:\033[0m Error of type compatibility ", type);
+      break;
+    case anotherSemanticErr:
+      fprintf( stderr, "\033[31;1mError %d:\033[0m Another semantic error ", type);
+      break;
+    default:
+      return;
+  }
+
+  fprintf( stderr, "\033[33m[%d:%d]\033[0m:\n", row, col);
+
+  unsigned int i = 1;
+  int consoleRowNum = 0;
+
+  while(i <= strlen(line)){
+    int j = 0;
+    for(; j < 80 && i <= strlen(line); j++){
+      printf("%c", line[i+j-1]);
     }
 
-    fprintf( stderr, "\033[33m[%d:%d]\033[0m:\n", row, col);
+    i += j;
 
-    unsigned int i = 1;
-    int consoleRowNum = 0;
+    printf("\n");
 
-    while(i <= strlen(line)){
-        int j = 0;
-        for(; j < 80 && i <= strlen(line); j++){
-            printf("%c", line[i+j-1]);
-        }
+    int rowFirstCharPosition = (consoleRowNum)*80 + 1;
 
-        i += j;
-
-        printf("\n");
-
-        int rowFirstCharPosition = (consoleRowNum)*80 + 1;
-
-        if(rowFirstCharPosition <= col && col < rowFirstCharPosition + 80){
-            for(int j = 1; j <= (col-1)%80; j++){
-                printf(" ");
-            }
-            printf("\x1B[31m^\x1B[0m\n");
-        }
+    if(rowFirstCharPosition <= col && col < rowFirstCharPosition + 80){
+      for(int j = 1; j <= (col-1)%80; j++){
+        printf(" ");
+      }
+      printf("\x1B[31m^\x1B[0m\n");
+    }
         
-        consoleRowNum++;
-    }
+    consoleRowNum++;
+  }
 
-    mmng_freeAll();
-    exit(type);
+  mmng_freeAll();
+  exit(type);
 }
