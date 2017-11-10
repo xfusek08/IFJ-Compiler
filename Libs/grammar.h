0f748c599c9da2378f4d4629f762345bb4820b1b
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2017-11-10 15:29:32 +0100

    LLGramatika(#16): Skoro hotová faktorizace s epsilon pravidly

--- Libs/grammar.h
+++ Libs/grammar.h
@@ -30,65 +30,98 @@ typedef enum
   ident, asng, eol, dataType,
 
   /* NON-TERMINALS */
-  NT_PROG,        // Program - staritng non-terminal
-  NT_DD,          // Declaration and definition
-  NT_SCOPE,       // program body
-  NT_STAT_LIST,   // statement list
-  NT_STAT,        // one statement
-  NT_PARAM_LIST,  // parameter list
-  NT_PARAM,       // one parameter
-  NT_EXPR_LIST,   // expresion list
-  NT_EXPR,        // expresion
-  NT_INIF         // body of if statement
+  NT_PROG,          // Program - staritng non-terminal
+  NT_ASSINGEXT,     // Assignement (...  [as datatype])
+  NT_DD_EXT,        // inicialization of static variable
+  NT_SCOPE,         // program body
+  NT_STAT_LIST,     // statement list
+  NT_STAT,          // one statement
+  NT_STAT_DOIN,     // body of do..loop statement
+  NT_STAT_DOIN_WU,  // [while/until]
+  NT_PARAM_LIST,    // parameter list
+  NT_PARAM,         // one parameter
+  NT_PARAM_EXT,     // continuous parameter (, param ,pram)
+  NT_EXPR_LIST,     // expresion list
+  NT_EXPR,          // expresion
+  NT_INIF           // body of if statement
 }EGrSymb;
 
 /*
 
-NT_PROG -> NT_DD NT_SCOPE
-NT_SCOPE -> kwScope NT_STAT_LIST kwEnd kwScope eol
+1. NT_PROG -> NT_DD NT_SCOPE
 
-NT_DD -> (epsilon)
-NT_DD -> kwDeclare kwFunction ident opLeftBrc NT_PARAM_LIST opRightBrc kwAs dataType eol NT_DD //programuj chytre
-NT_DD -> kwFunction ident opLeftBrc NT_PARAM_LIST opRightBrc kwAs dataType eol NT_STAT_LIST kwEnd kwFunction eol NT_DD
-NT_DD -> kwStatic kwShared ident kwAs dataType
-NT_DD -> kwStatic kwShared ident kwAs dataType asgn NT_EXPR
+first(NT_DD) = { kwDeclare -> (2); kwFunction -> (3);  kwStatic -> (4); else -> (5 [epsilon]) }
+2. NT_DD -> kwDeclare kwFunction ident opLeftBrc NT_PARAM_LIST opRightBrc kwAs dataType eol NT_DD
+3. NT_DD -> kwFunction ident opLeftBrc NT_PARAM_LIST opRightBrc kwAs dataType eol NT_STAT_LIST kwEnd kwFunction eol NT_DD
+4. NT_DD -> kwStatic kwShared ident kwAs dataType NT_DD_EXT
+5. NT_DD -> (epsilon)
 
-NT_PARAM_LIST -> (epsilon)
-NT_PARAM_LIST -> NT_PARAM
+first(NT_ASSINGEXT) = { asgn -> (6); else -> (7 [epsilon]) }
+6. NT_ASSINGEXT -> asgn NT_EXPR
+7. NT_ASSINGEXT -> (epsilon)
 
-NT_PARAM -> ident kwAs dataType
-NT_PARAM -> ident kwAs dataType opComma NT_PARAM
+first(NT_SCOPE) = { kwScope -> (8); else -> (error) }
+8. NT_SCOPE -> kwScope NT_STAT_LIST kwEnd kwScope eol
 
-NT_STAT_LIST -> (epsilon)
-NT_STAT_LIST -> NT_STAT eol NT_STAT_LIST
+first(NT_SCOPE) = { first(NT_PARAM) -> (9); else -> (10 [epsilon]) }
+9.  NT_PARAM_LIST -> NT_PARAM
+10. NT_PARAM_LIST -> (epsilon)
 
-NT_STAT -> kwDim iden kwAs dataType
-NT_STAT -> kwDim iden kwAs dataType asgn NT_EXPR
-NT_STAT -> ident asng NT_EXPR
-NT_STAT -> kwInput ident
-NT_STAT -> kwPrint NT_EXPR_LIST
-NT_STAT -> kwIf NT_EXPR kwThan eol NT_INIF kwEnd kwIf eol
-NT_STAT -> kwDo kwWhile NT_EXPR eol NT_STAT_LIST kwLoop
-NT_STAT -> kwDo kwUntil NT_EXPR eol NT_STAT_LIST kwLoop
-NT_STAT -> kwDo eol NT_STAT_LIST kwWhile NT_EXPR kwLoop
-NT_STAT -> kwDo eol NT_STAT_LIST kwUntil NT_EXPR kwLoop
+first(NT_PARAM) = { ident -> (11); else -> (error) }
+11. NT_PARAM -> ident kwAs dataType NT_PARAM_EXT
 
-NT_STAT -> kwContinue
-NT_STAT -> kwExit
-NT_STAT -> NT_SCOPE
-NT_STAT -> kwReturn NT_EXPR
+first(NT_PARAM_EXT) = { opComma -> (12); else -> (13 [epsilon]) }
+12. NT_PARAM_EXT -> opComma NT_PARAM
+13. NT_PARAM_EXT -> (epsilon)
 
-NT_STAT -> kwFor NT_FOR_ITER asgn NT_EXPR kwTo NT_EXPR NT_STEP eol NT_STAT_LIST kwNext // ?? kwTo pridano
+first(NT_STAT_LIST) = { first(NT_STAT) -> (14); else -> (15 [epsilon]) }
+14. NT_STAT_LIST -> NT_STAT eol NT_STAT_LIST
+15. NT_STAT_LIST -> (epsilon)
 
-NT_FOR_ITER -> ident
-NT_FOR_ITER -> ident kwAs dataType
+first(NT_STAT) = {
+  kwInput -> (16);
+  kwPrint -> (17);
+  kwIf -> (18);
+  kwDim -> (19);
+  ident -> (20);
+  kwContinue -> (21);
+  kwExit -> (22);
+  first(NT_SCOPE) -> (23);
+  kwReturn -> (24);
+  kwDo -> (25);
+  kwFor -> (26);
+  else -> (error) }
+16. NT_STAT -> kwInput ident
+17. NT_STAT -> kwPrint NT_EXPR_LIST
+18. NT_STAT -> kwIf NT_EXPR kwThan eol NT_INIF kwEnd kwIf eol
+19. NT_STAT -> kwDim iden kwAs dataType NT_ASSINGEXT
+20. NT_STAT -> ident asng NT_EXPR
+21. NT_STAT -> kwContinue
+22. NT_STAT -> kwExit
+23. NT_STAT -> NT_SCOPE
+24. NT_STAT -> kwReturn NT_EXPR
+25. NT_STAT -> kwDo NT_DOIN kwLoop
+26. NT_STAT -> kwFor ident NT_ASSINGEXT kwTo NT_EXPR NT_STEP eol NT_STAT_LIST kwNext
 
-NT_STEP -> (epsilon)
-NT_STEP -> kwStep NT_EXPR
+first(NT_DOIN) = { first(NT_DOIN_WU) -> (27); eol -> (28) else -> (error)}
+27. NT_DOIN -> NT_DOIN_WU NT_EXPR eol NT_STAT_LIST
+28. NT_DOIN -> eol NT_STAT_LIST NT_DOIN_WU NT_EXPR
 
-NT_INIF -> NT_STAT_LIST
-NT_INIF -> NT_STAT_LIST kwElse eol NT_STAT_LIST
-NT_INIF -> NT_STAT_LIST kwElsif NT_EXPR kwThan eol NT_INIF
+first(NT_DOIN_WU) = { kwWhile -> (29); kwUntil -> (30); else -> (error) }
+29. NT_DOIN_WU -> kwWhile
+30. NT_DOIN_WU -> kwUntil
+
+first(NT_FORSTEP) = { kwStep -> (31); else -> (32 [epsilon]) }
+31. NT_FORSTEP -> kwStep NT_EXPR
+32. NT_FORSTEP -> (epsilon)
+
+first(NT_INIF) = { fist(NT_STAT_LIST) -> (33); else -> (32 [epsilon]) }
+33. NT_INIF -> NT_STAT_LIST NT_INIF_EXT
+
+first(NT_INIF_EXT) = { kwElsif -> (34); kwElse -> (35); else -> (36 [epsilon]) }
+34. NT_INIF_EXT -> kwElsif NT_EXPR kwThan eol NT_INIF
+35. NT_INIF_EXT -> kwElse eol NT_STAT_LIST
+36. NT_INIF_EXT -> (epsilon)
 
 NT_EXPR_LIST -> (epsilon)
 NT_EXPR_LIST -> NT_EXPR opSemcol NT_EXPR_LIST
