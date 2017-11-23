4959e35cbaa4d0165db846813da0be683928c972
Pavel Vosyka <pavel.7820q@gmail.com>
2017-11-23 15:22:07 +0100

    PrecedentParser(#6): implementace

--- Libs/syntaxAnalyzer.c
+++ Libs/syntaxAnalyzer.c
@@ -587,7 +587,7 @@ SToken sytx_getFreeVar()
   if (symbt_findSymb(ident) == NULL)
   {
     symbt_insertSymbOnTop(ident);
-    printf("DEFVAR %s", ident);
+    printf("DEFVAR %s\n", ident);
   }
   token.symbol->ident = ident;
   return token;
@@ -718,6 +718,7 @@ TSymbol syntx_processExpression(SToken *actToken, TSymbol symbol)
     }
     if (!syntx_getPrecedence(terminal, actToken->type, &tablesymb))
     {
+      DPRINT("Table: undefined precedence!.");
       scan_raiseCodeError(syntaxErr);
     }
     DDPRINT("Table: %d", tablesymb);
