1789fc829848ea946f7bf374068a49ff1d92e7d3
xfrane16 <Jara.Franek2@gmail.com>
2017-11-28 15:29:24 +0100

    SyntaxAnalyzerComb#24: Přesun
    	-Přesunutí definice tokenu z scanner.c do utils.c

--- Libs/utils.h
+++ Libs/utils.h
@@ -26,6 +26,15 @@ typedef enum {
 } DataType;
 
 /**
+ * Struct representing one token of analysis
+ */
+typedef struct {
+  EGrSymb type;       /*!< terminal lextype from grammar */
+  TSymbol symbol;     /*!< Symbol evided in symbol table, NULL if there is no need of additional information */
+  DataType dataType;  /*!< This attribute is used only if token type is dataType (we need remember wich data type) and symbol of such of token is unnecessary */
+} SToken;
+
+/**
  * Fuction create hard copy of given string
  * \note Memory is allocated here and free has to be called.
  */
