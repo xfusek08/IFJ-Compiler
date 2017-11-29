4065b2eb7869ce6c4f892b465c4376da1416935b
xfrane16 <Jara.Franek2@gmail.com>
2017-11-29 14:59:05 +0100

    origin/Built-inFunctions#21: Úprava výpisu
    	-Funkce by nyní měli být správně vypsány.

--- Libs/utils.c
+++ Libs/utils.c
@@ -51,66 +51,18 @@ const char *util_dataTypeToString(const DataType dataType)
 // Fuction prints build-in functions
 void util_PrintBuildFunc()
 {
-  printf("LABEL $$Length
-  PUSHFRAME 
-  DEFVAR LF@%retval
-  MOVE LF@%retval int@0
-  STRLEN LF@%retval LF@p1 
-  POPFRAME
-  RETURN");
+  printf("LABEL $$Length \nPUSHFRAME \nDEFVAR LF@\037retval \nMOVE LF@\037retval int@0 \nSTRLEN LF@\037retval LF@p1 \nPOPFRAME \nRETURN \n");
   
-  printf("LABEL $$SubStr
-  PUSHFRAME
-  DEFVAR LF@%retval
-  DEFVAR LF@len
-  DEFVAR LF@help
-  STRLEN LF@len LF@p1
-  MOVE LF@%retval string@
-  JUMPIFEQ $$EndSubStr LF@%retval LF@p1
-  GT LF@help int@1 LF@p2
-  JUMPIFEQ $$EndSubStr LF@help bool@true
-  GT LF@help int@0 LF@p3
-  JUMPIFEQ $$SubStrExtra1 LF@help bool@true
-  SUB LF@help LF@len LF@p2
-  GT LF@help LF@p3 LF@help
-  JUMPIFEQ $$SubStrExtra2 LF@help bool@true 
-  JUMP $$SubStrStart
-LABEL $$SubStrExtra1
-  MOVE LF@p2 LF@len
-  JUMP $$SubStrStart
-LABEL $$SubStrExtra2
-  MOVE LF@p3 LF@len
-  SUB LF@p3 LF@p3 LF@p2
-LABEL $$SubStrStart
-  ADD LF@p3 LF@p2 LF@p3
-LABEL $$CycleSubStr 
-  GETCHAR LF@help LF@p1 LF@p2
-  CONCAT LF@%retval LF@%retval LF@help
-  ADD LF@p2 LF@p2 int@1
-  JUMPIFNEQ $$CycleSubStr LF@p2 LF@p3
-LABEL $$EndSubStr 
-  POPFRAME
-  RETURN");
+  printf("\nLABEL $$SubStr \nPUSHFRAME \nDEFVAR LF@\037retval \nDEFVAR LF@len \nDEFVAR LF@help \nSTRLEN LF@len LF@p1 \nMOVE LF@\037retval string@\n");
+  printf("JUMPIFEQ $$EndSubStr LF@\037retval LF@p1 \nGT LF@help int@1 LF@p2 \nJUMPIFEQ $$EndSubStr LF@help bool@true \nGT LF@help int@0 LF@p3\n");
+  printf("JUMPIFEQ $$SubStrExtra1 LF@help bool@true \nSUB LF@help LF@len LF@p2 \nGT LF@help LF@p3 LF@help \nJUMPIFEQ $$SubStrExtra2 LF@help bool@true\n"); 
+  printf("JUMP $$SubStrStart \nLABEL $$SubStrExtra1 \nMOVE LF@p2 LF@len \nJUMP $$SubStrStart \nLABEL $$SubStrExtra2 \nMOVE LF@p3 LF@len\n");
+  printf("SUB LF@p3 LF@p3 LF@p2 \nLABEL $$SubStrStart \nADD LF@p3 LF@p2 LF@p3 \nLABEL $$CycleSubStr \nGETCHAR LF@help LF@p1 LF@p2\n");
+  printf("CONCAT LF@\037retval LF@\037retval LF@help \nADD LF@p2 LF@p2 int@1 \nJUMPIFNEQ $$CycleSubStr LF@p2 LF@p3 \nLABEL $$EndSubStr \nPOPFRAME \nRETURN \n");
   
-  printf("LABEL $$Asc
-  PUSHFRAME 
-  DEFVAR LF@%retval
-  DEFVAR LF@help
-  MOVE LF@%retval int@0
-  STRLEN LF@help LF@p1
-  GT LF@help LF@help LF@p2
-JUMPIFEQ $$EndAsc LF@help bool@false 
-  GETCHAR LF@%retval LF@p1 LF@p2
-  STRI2INT LF@%retval LF@%retval int@0
-LABEL $$EndAsc  
-  POPFRAME
-  RETURN");
+  printf("\nLABEL $$Asc \nPUSHFRAME \nDEFVAR LF@\037retval \nDEFVAR LF@help \nMOVE LF@\037retval int@0 \nSTRLEN LF@help LF@p1 \nGT LF@help LF@help LF@p2\n");
+  printf("JUMPIFEQ $$EndAsc LF@help bool@false \nGETCHAR LF@\037retval LF@p1 LF@p2 \nSTRI2INT LF@\037retval LF@\037retval int@0 \nLABEL $$EndAsc \nPOPFRAME\n");
+  printf("RETURN\n");
   
-  printf("LABEL $$Chr
-  PUSHFRAME 
-  DEFVAR LF@%retval
-  MOVE LF@%retval string@
-  INT2CHAR LF@%retval LF@p1
-  POPFRAME
-  RETURN");
+  printf("\nLABEL $$Chr \nPUSHFRAME \nDEFVAR LF@\037retval \nMOVE LF@\037retval string@ \nINT2CHAR LF@\037retval LF@p1 \nPOPFRAME \nRETURN");
 }
